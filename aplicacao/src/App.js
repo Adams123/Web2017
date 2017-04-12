f4c04be51e7d14d0edd9f3b966d605e71f6fd5ed
Adams Silva <adamsvcs@gmail.com>
2017-04-11 22:42:29 -0300

    Teoricamente era pra funcionar interface

--- aplicacao/src/App.js
+++ aplicacao/src/App.js
@@ -1,20 +1,97 @@
 import React from 'react';
-import './App.css';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-            <div>
-            <form>
-              <input placeholder="Nome">
-              </input>
-              <button type="submit">add</button>
-            </form>
-          </div>
-        </div>
+var Itens = React.createClass({
+    createTasks: function (item)
+    {
+        return <li key =
+            {
+                item.key
+            } > {
+                item.text
+            } < /li>
+    },
+
+    render: function ()
+    {
+        var todoEntries = this.props.entries;
+
+        function createTasks(item)
+        {
+            return <li key =
+                {
+                    item.key
+                } > {
+                    item.text
+                } < /li>
+        }
+
+        var listItems = todoEntries.map(createTasks);
+
+        return ( <
+            ul className = "theList" >
+                {
+                listItems
+                } <
+            /ul>
         );
     }
-}
+});
+
+var App = React.createClass({
+    getInitialState: function ()
+    {
+        return {
+            items: []
+        };
+    },
+    addItem: function (e)
+    {
+        var itemArray = this.state.items;
+
+        itemArray.push(
+            {
+            text: this._inputElement.value,
+            key: Date.now()
+            });
+
+        this.setState(
+            {
+            items: itemArray
+            });
+
+        this._inputElement.value = "";
+
+        e.preventDefault();
+    },
+    render: function ()
+    {
+        return ( <
+            div className = "todoListMain" >
+            <
+            div className = "header" >
+            <
+            form onSubmit = {
+                this.addItem
+            } >
+            <
+            input ref =
+                {
+                (a) => this._inputElement = a
+                }
+            placeholder = "enter task" >
+            <
+            /input> <
+            button type = "submit" > add < /button> <
+            /form> <
+            /div> <
+            Itens entries =
+            {
+                this.state.items
+            }
+            /> <
+            /div>
+        );
+    }
+});
 
 export default App;
--- aplicacao/src/index.js
+++ aplicacao/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import TodoList from './App';
+import Items from './items'
 import './index.css';
 
 ReactDOM.render(
