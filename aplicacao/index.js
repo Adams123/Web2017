09f529da04ba7858b81dcf81c61c7a8c607d2baf
antoniop-lm <antoniop.lm@hotmail.com>
2017-04-14 23:15:47 -0300

    Jeito porco de fazer o navigation

--- aplicacao/index.js
+++ aplicacao/index.js
@@ -1,3 +1,97 @@
+function showLogin() {
+	// var node = document.createElement("FORM");
+	// node.id = "formLogin";
+	// node.createTextNode("Login:");
+	// var input = document.createElement("INPUT");
+	// input.type = "text";
+	// input.name = "login";
+	// input.id = "user";
+	// node.appendChild(input);
+	// var br = document.createElement("BR");
+	// node.appendChild(br);
+	// node.createTextNode("Senha:");
+	// var input = document.createElement("INPUT");
+	// input.type = "password";
+	// input.id = "senha";
+	// node.appendChild(input);
+	// var br = document.createElement("BR");
+	// node.appendChild(br);
+	// var button = document.createElement("BUTTON");
+	// button.onclick = "logon(document.getElementById('user').value, document.getElementById('senha').value)";
+	// button.createTextNode("Entrar");
+	// node.appendChild(button);
+	
+	// let login = document.getElementById("login");
+	// login.appendChild(node);
+	// node.submit();
+}
+
+function navCliente() {
+	let navBar = document.getElementById("navigationBar");
+	navBar.removeChild(navBar.childNodes[4]);
+	navBar.removeChild(navBar.childNodes[3]);
+	
+	var node = document.createElement("LI");
+	var a = document.createElement("A");
+	a.id = "produtos";
+	a.href = "#";
+	var texta = document.createTextNode("Produtos");
+	a.appendChild(texta);
+	node.appendChild(a);
+	navBar.appendChild(node);
+	
+	var node = document.createElement("LI");
+	var a = document.createElement("A");
+	a.id = "calendario";
+	a.href = "#";
+	var texta = document.createTextNode("Calendário");
+	a.appendChild(texta);
+	node.appendChild(a);
+	navBar.appendChild(node);
+
+	var node = document.createElement("LI");
+	var a = document.createElement("A");
+	a.id = "login";
+	a.href = "#";
+	var texta = document.createTextNode("Profile");
+	a.appendChild(texta);
+	node.appendChild(a);
+	navBar.appendChild(node);
+}
+
+function navAdmin() {
+	let navBar = document.getElementById("navigationBar");
+	navBar.removeChild(navBar.childNodes[4]);
+	navBar.removeChild(navBar.childNodes[3]);
+	
+	var node = document.createElement("LI");
+	var a = document.createElement("A");
+	a.id = "cadastrar";
+	a.href = "#";
+	var texta = document.createTextNode("Cadastrar");
+	a.appendChild(texta);
+	node.appendChild(a);
+	navBar.appendChild(node);
+	
+	var node = document.createElement("LI");
+	var a = document.createElement("A");
+	a.id = "servicos";
+	a.href = "#";
+	var texta = document.createTextNode("Serviços");
+	a.appendChild(texta);
+	node.appendChild(a);
+	navBar.appendChild(node);
+
+	var node = document.createElement("LI");
+	var a = document.createElement("A");
+	a.id = "login";
+	a.href = "#";
+	var texta = document.createTextNode("Profile");
+	a.appendChild(texta);
+	node.appendChild(a);
+	navBar.appendChild(node);	
+}
+
 function logon(nomeCliente, senhaCliente) {
 	let divLogin = document.getElementById("formLogin");
 	divLogin.outerHTML = "";
@@ -12,9 +106,11 @@ function logon(nomeCliente, senhaCliente) {
 function loginAdmin(){
 	let divAdmin = document.getElementById("admin");
 	divAdmin.innerHTML = "<h1>Bem vindo, Admin</h1>";
+	navAdmin();
 }
 
 function loginCliente(nomeCliente){
 	let divCliente = document.getElementById("cliente");
 	divCliente.innerHTML = "<h1>Bem vindo, "+ nomeCliente + "</h1>";
+	navCliente();
 }
