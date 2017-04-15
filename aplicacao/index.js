8b282af571addb0824a7ce4f960827f016c1a32a
Adams Silva <adamsvcs@gmail.com>
2017-04-15 03:28:15 -0300

    interface refeita quase q inteira, mais fácil de mexer

--- aplicacao/index.js
+++ aplicacao/index.js
@@ -1,87 +1,51 @@
 function closeAll() {
-	document.getElementById("formLogin").style.display = 'none';
-	document.getElementById("bemvindo").style.display = 'none';
-	document.getElementById("cliente").style.display = 'none';
-	document.getElementById("admin").style.display = 'none';
-	document.getElementById("confirmacao").style.display = 'none';
-	document.getElementById("pets").style.display = 'none';
-}
-
-function showLogin() {
-	closeAll();
-	document.getElementById("formLogin").style.display = 'inline-block';
+    document.getElementById("bemvindo").style.display = 'none';
+    document.getElementById("cliente").style.display = 'none';
+    document.getElementById("admin").style.display = 'none';
+    document.getElementById("confirmacao").style.display = 'none';
+    document.getElementById("pets").style.display = 'none';
 }
 
 function confirmLogout(choice) {
-	if (choice == true) {
-		document.getElementById("produtos").style.display = 'none';
-		document.getElementById("calendario").style.display = 'none';
-		document.getElementById("cadastrar").style.display = 'none';
-		document.getElementById("servicos").style.display = 'none';
-		document.getElementById("login").style.display = 'inline-block';
-		document.getElementById("profile").style.display = 'none';
-		document.getElementById("logout").style.display = 'none';
-	}
-
-	showHome();
+    if (choice == true)
+    {
+        $("#navBarHome").show();
+        $("#navBarAdmin").hide();
+        $("#navBarCliente").hide();
+    }
+    else
+    {
+        $('.confirm').hide();
+    }
 }
 
 function showProfile() {
-	closeAll();
-	document.getElementById("pets").style.display = 'inline-block';
-}
-
-function showLogout() {
-	closeAll();
-	document.getElementById("confirmacao").style.display = 'inline-block';
-}
-
-function showHome() {
-	closeAll();
-	document.getElementById("bemvindo").style.display = 'inline-block';
+    closeAll();
+    document.getElementById("pets").style.display = 'inline-block';
 }
 
 function navCliente() {
-	document.getElementById("produtos").style.display = 'inline-block';
-	document.getElementById("calendario").style.display = 'inline-block';
-	document.getElementById("login").style.display = 'none';
-	document.getElementById("profile").style.display = 'inline-block';
-	document.getElementById("logout").style.display = 'inline-block';
-}
-
-function navAdmin() {
-	document.getElementById("cadastrar").style.display = 'inline-block';
-	document.getElementById("servicos").style.display = 'inline-block';
-	document.getElementById("login").style.display = 'none';
-	document.getElementById("profile").style.display = 'inline-block';
-	document.getElementById("logout").style.display = 'inline-block';
+    $("#produtos").style.display = 'inline-block';
+    $("#calendario").style.display = 'inline-block';
+    $("#login").style.display = 'none';
+    $("#profile").style.display = 'inline-block';
+    $("#logout").style.display = 'inline-block';
 }
 
 function logon(nomeCliente, senhaCliente) {
-	closeAll();
-
-	if (nomeCliente == 'admin' && senhaCliente == 'admin'){
-		loginAdmin();
-	}else{
-		loginCliente(nomeCliente);
-	}
+    if (nomeCliente == 'admin' && senhaCliente == 'admin') {
+        loginAdmin();
+    } else {
+        loginCliente(nomeCliente);
+    }
 }
 
 function loginAdmin(){
-	let divAdmin = document.getElementById("admin");
-	divAdmin.innerHTML = "<h1>Bem vindo, Admin</h1>";
-	divAdmin.style.display = 'inline-block';
-	navAdmin();
+        $("#navBarHome").hide();
+        $("#navBarAdmin").show();
 }
-
-function loginCliente(nomeCliente){
-	let divCliente = document.getElementById("cliente");
-	divCliente.innerHTML = "<h1>Bem vindo, "+ nomeCliente + "</h1>";
-	divCliente.style.display = 'inline-block';
-	navCliente();
+function loginCliente(nomeCliente) {
+    let divCliente = document.getElementById("homeCliente");
+    $("#navBarCliente").show();
+    $("#navBarHome").hide();
 }
-//faz com que quando clique em uma tab do admin ela exiba o conteudo
-$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-    var target = this.href.split('#');
-    $('.nav a').filter('[href="#' + target[1] + '"]').tab('show');
-})
