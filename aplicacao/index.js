653aeb46b53ab9e35248fe82365cac90624179e0
Adams Silva <adamsvcs@gmail.com>
2017-04-15 04:01:49 -0300

    melhorado a interface e fluxo de dados, corrigido alguns bugs, organizado alguns arquivos (css no .css, scripts no .js)

--- aplicacao/index.js
+++ aplicacao/index.js
@@ -24,14 +24,6 @@ function showProfile() {
     document.getElementById("pets").style.display = 'inline-block';
 }
 
-function navCliente() {
-    $("#produtos").style.display = 'inline-block';
-    $("#calendario").style.display = 'inline-block';
-    $("#login").style.display = 'none';
-    $("#profile").style.display = 'inline-block';
-    $("#logout").style.display = 'inline-block';
-}
-
 function logon(nomeCliente, senhaCliente) {
     if (nomeCliente == 'admin' && senhaCliente == 'admin') {
         loginAdmin();
@@ -49,3 +41,41 @@ function loginCliente(nomeCliente) {
     $("#navBarCliente").show();
     $("#navBarHome").hide();
 }
+
+
+$(document).ready(function() {
+            $('.calendar').fullCalendar({
+                weekends: false,
+                defaultView: 'agendaWeek',
+                businessHours: {
+                    dow: [1, 2, 3, 4, 5], // Monday - Friday
+                    start: '08:00',
+                    end: '18:00',
+                },
+                allDaySlot: false,
+                slotDuration: '01:00:00',
+                events: [{
+                        title: 'Vacina Pet',
+                        start: '2017-04-12T08:00:00',
+                        end: '2017-04-12T09:00:00',
+                        allDay: false // will make the time show
+                    },
+                    {
+                        title: 'Banho Pet',
+                        start: '2017-04-10T12:00:00',
+                        end: '2017-04-10T13:00:00',
+                        allDay: false // will make the time show
+                    }
+                ],
+                dayClick: function(date, jsEvent, view) {
+                    alert('Clicked on: ' + date.format('DD/MM/YYYY HH:mm'));
+                    //events.push({title  : 'Banho Pet',
+                    //    start  : '2017-04-13T12:00:00',
+                    //   end    : '2017-04-13T13:00:00'});
+                    //alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+                    //alert('Current view: ' + view.name);
+                    // change the day's background color just for fun
+                    //$(this).css('background-color', 'red');
+                }
+            })
+        });
