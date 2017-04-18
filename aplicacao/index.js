0507c83df55c9f2a5f50be5ab54c4b24bafee98e
Adams Silva <adamsvcs@gmail.com>
2017-04-17 21:57:48 -0300

    relatório feito + pequenas alterações

--- aplicacao/index.js
+++ aplicacao/index.js
@@ -4,15 +4,18 @@
 // * Gustavo Dutra Santana - 8532040
 // * Veronica Vannini - 8517369
 
+// Funcao para mudar a visualização ao fazer o logout
 function confirmLogout(choice) {
-    if (choice == true)
-    {
+    if (choice == true) {
         $("#navBarHome").show();
         $("#navBarAdmin").hide();
         $("#navBarCliente").hide();
+    } else {
+
     }
 }
 
+// verificacao de admin ou cliente
 function logon(nomeCliente, senhaCliente) {
     if (nomeCliente == 'admin' && senhaCliente == 'admin') {
         loginAdmin();
@@ -21,137 +24,138 @@ function logon(nomeCliente, senhaCliente) {
     }
 }
 
-function loginAdmin(){
+
+//configura exibicao para admin
+function loginAdmin() {
     $("#navBarAdmin").show();
     $("#navBarHome").hide();
 }
-
+//configura exibicao para cliente
 function loginCliente(nomeCliente) {
     $("#navBarCliente").show();
     $("#navBarHome").hide();
 }
 
+//toggable e toggable2 são variáveis usadas para realizar a navegação entre as abas, sendo associadas às abas e seus respectivos conteúdos
+$(document).ready(function () {
+    var toggable = $("[data-toggable]");
+    $("[data-toggle]").each(function () {
+        var tab = $($(this).attr('href'));
+        $(this).click(function () {
+            toggable.hide();
+            tab.show();
+        });
+    });
 
-$(document).ready(function() {
-	var toggable = $("[data-toggable]");
-	$("[data-toggle]").each(function(){
-		var tab = $($( this ).attr('href'));
-		$( this ).click(function(){
-		  toggable.hide();
-		  tab.show();
-		});
-	});
+    toggable.hide();
+    $("#home").show();
 
-	toggable.hide();
-	$("#home").show();
-	
-	var toggable2 = $("[data-toggable2]");
-	$("[data-toggle2]").each(function(){
-		var tab = $($( this ).attr('href'));
-		$( this ).click(function(){
-		  toggable2.hide();
-		  tab.show();
-		});
-	});
+    var toggable2 = $("[data-toggable2]");
+    $("[data-toggle2]").each(function () {
+        var tab = $($(this).attr('href'));
+        $(this).click(function () {
+            toggable2.hide();
+            tab.show();
+        });
+    });
 
-	toggable2.hide();
-  
-	$('.calendar').fullCalendar({
-		weekends: false,
-		defaultView: 'agendaWeek',
-		businessHours: {
-			dow: [1, 2, 3, 4, 5], // Monday - Friday
-			start: '08:00',
-			end: '18:00',
-		},
-		allDaySlot: false,
-	   // slotDuration: '01:00:00',
-		events: [{
-				title: 'Vacina Pet - Pet: Pipoca',
+    toggable2.hide();
+
+
+
+    //---------------- customizacao do plugin do calendário
+    $('.calendar').fullCalendar({
+        weekends: false,
+        defaultView: 'agendaWeek',
+        businessHours: {
+            dow: [1, 2, 3, 4, 5], // Monday - Friday
+            start: '08:00',
+            end: '18:00',
+        },
+        allDaySlot: false,
+        // slotDuration: '01:00:00',
+        events: [{
+                title: 'Vacina Pet - Pet: Pipoca',
                 url: 'https://www.veterinariamatogrosso.com.br/uploads/images/2016/12/6-1480591557.jpg',
-				start: '2017-04-12T08:00:00',
-				end: '2017-04-12T09:00:00',
-				allDay: false // will make the time show
+                start: '2017-04-12T08:00:00',
+                end: '2017-04-12T09:00:00',
+                allDay: false
 			},
-			{
-				title: 'Banho Pet - Pet: Godofredo',
+            {
+                title: 'Banho Pet - Pet: Godofredo',
                 url: 'http://petshopbigdogsbrasil.com.br/site/uploads/banho-e-tosa/banho-medicamentoso/banho-cachorro-petitobi.jpg',
-			   start: '2017-04-10T12:00:00',
-			   end: '2017-04-10T13:00:00',
-			   allDay: false // will make the time show
+                start: '2017-04-10T12:00:00',
+                end: '2017-04-10T13:00:00',
+                allDay: false
 		   },
-           {
-				title: 'Vacina Pet - Pet: Pipoca',
+            {
+                title: 'Vacina Pet - Pet: Pipoca',
                 url: 'https://www.veterinariamatogrosso.com.br/uploads/images/2016/12/6-1480591557.jpg',
-				start: '2017-04-17T08:00:00',
-				end: '2017-04-17T09:00:00',
-				allDay: false // will make the time show
+                start: '2017-04-17T08:00:00',
+                end: '2017-04-17T09:00:00',
+                allDay: false
 			},
-			{
-				title: 'Banho Pet - Pet: Godofredo',
+            {
+                title: 'Banho Pet - Pet: Godofredo',
                 url: 'http://petshopbigdogsbrasil.com.br/site/uploads/banho-e-tosa/banho-medicamentoso/banho-cachorro-petitobi.jpg',
-			   start: '2017-04-19T12:00:00',
-			   end: '2017-04-19T13:00:00',
-			   allDay: false // will make the time show
+                start: '2017-04-19T12:00:00',
+                end: '2017-04-19T13:00:00',
+                allDay: false
 		   },
-           {
-				title: 'Vacina Pet - Pet: Pipoca',
+            {
+                title: 'Vacina Pet - Pet: Pipoca',
                 url: 'https://www.veterinariamatogrosso.com.br/uploads/images/2016/12/6-1480591557.jpg',
-				start: '2017-04-24T08:00:00',
-				end: '2017-04-24T09:00:00',
-				allDay: false // will make the time show
+                start: '2017-04-24T08:00:00',
+                end: '2017-04-24T09:00:00',
+                allDay: false
 			},
-			{
-				title: 'Banho Pet - Pet: Godofredo',
+            {
+                title: 'Banho Pet - Pet: Godofredo',
                 url: 'http://petshopbigdogsbrasil.com.br/site/uploads/banho-e-tosa/banho-medicamentoso/banho-cachorro-petitobi.jpg',
-			   start: '2017-04-26T12:00:00',
-			   end: '2017-04-26T13:00:00',
-			   allDay: false // will make the time show
+                start: '2017-04-26T12:00:00',
+                end: '2017-04-26T13:00:00',
+                allDay: false
 		   },
-           {
-				title: 'Vacina Pet - Pet: Pipoca',
+            {
+                title: 'Vacina Pet - Pet: Pipoca',
                 url: 'https://www.veterinariamatogrosso.com.br/uploads/images/2016/12/6-1480591557.jpg',
-				start: '2017-05-01T08:00:00',
-				end: '2017-05-01T09:00:00',
-				allDay: false // will make the time show
+                start: '2017-05-01T08:00:00',
+                end: '2017-05-01T09:00:00',
+                allDay: false
 			},
-			{
-				title: 'Banho Pet - Pet: Godofredo',
+            {
+                title: 'Banho Pet - Pet: Godofredo',
                 url: 'http://petshopbigdogsbrasil.com.br/site/uploads/banho-e-tosa/banho-medicamentoso/banho-cachorro-petitobi.jpg',
-			   start: '2017-05-04T12:00:00',
-			   end: '2017-05-04T13:00:00',
-			   allDay: false // will make the time show
+                start: '2017-05-04T12:00:00',
+                end: '2017-05-04T13:00:00',
+                allDay: false
 		   },
-           {
-				title: 'Vacina Pet - Pet: Pipoca',
+            {
+                title: 'Vacina Pet - Pet: Pipoca',
                 url: 'https://www.veterinariamatogrosso.com.br/uploads/images/2016/12/6-1480591557.jpg',
-				start: '2017-05-09T08:00:00',
-				end: '2017-05-09T09:00:00',
-				allDay: false // will make the time show
+                start: '2017-05-09T08:00:00',
+                end: '2017-05-09T09:00:00',
+                allDay: false
 			},
-			{
-				title: 'Banho Pet - Pet: Godofredo',
+            {
+                title: 'Banho Pet - Pet: Godofredo',
                 url: 'http://petshopbigdogsbrasil.com.br/site/uploads/banho-e-tosa/banho-medicamentoso/banho-cachorro-petitobi.jpg',
-			   start: '2017-05-12T12:00:00',
-			   end: '2017-05-12T13:00:00',
-			   allDay: false // will make the time show
+                start: '2017-05-12T12:00:00',
+                end: '2017-05-12T13:00:00',
+                allDay: false
 		   }
 		],
-		dayClick: function(date, jsEvent, view) {
-			alert('Clicked on: ' + date.format('DD/MM/YYYY HH:mm'));
-			
-			// Get the modal
-			var modal = document.getElementById('myModal');
-		   // modal.getElementB('p').innerHTML = date.format('DD/MM/YYYY HH:mm');​
-			modal.style.display = "block";
+        dayClick: function (date, jsEvent, view) {
+            alert('Clicked on: ' + date.format('DD/MM/YYYY HH:mm'));
 
-			// Get the <span> element that closes the modal
-			var span = document.getElementsByClassName("close")[0];
-			span.onclick = function() {
-				modal.style.display = "none";
-			}
+            var modal = document.getElementById('myModal');
+            modal.style.display = "block";
 
-		}
-	})
+            var span = document.getElementsByClassName("close")[0];
+            span.onclick = function () {
+                modal.style.display = "none";
+            }
+
+        }
+    })
 });
-
