d63a2af36ecf1e1db97e528ae7921cd0f1b2583f
Adams Silva <adamsvcs@gmail.com>
2017-07-09 22:27:46 -0300

    servicos + relatorio

--- petshop/src/containers/Services.js
+++ petshop/src/containers/Services.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from 'react';
+import NavLink from '../util/NavLink';
 
 export default React.createClass({
   render() {
-    return <div>Serviços</div>
-  }
-})
+    return(
+        <div>
+        <div className="service-menu">
+        <ul role="nav">
+            <li><NavLink to="/services/insert">Inserir</NavLink></li>
+            <li><NavLink to="/services/show">Exibir</NavLink></li>
+        </ul>
+        </div>
+    <form id="form-new-service" className="formSubmit" onSubmit={this._handleNewService}>
+          <ul>
+            <li>
+              <input type="text" placeholder="Nome do Serviço"/>{' '}
+            </li>
+            <li>
+              <input type="text" placeholder="Descrição"/>{' '}
+            </li>
+            <li>
+              <input type="number" placeholder="Preço"/>{' '}
+            </li>
+            <li>
+              <input type="file" placeholder="Foto"/>{' '}
+            </li>
+          <button type="submit">Inserir</button>
+          </ul>
+    </form>
+    <table id="services-table" className="tabelas">
+        <thead>
+            <th>Nome</th>
+            <th>Descrição</th>
+            <th>Preço</th>
+            <th>Foto</th>
+        </thead>
+        <tbody id="services-table-body">
+        </tbody>
+    </table>
+        </div>
+    );
+}})
--- petshop/webpack.config.js
+++ petshop/webpack.config.js
@@ -1,30 +1,29 @@
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './index.js',
+    entry: './index.js',
 
-  output: {
-    path: 'public',
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/public'
+    },
 
-  plugins: process.env.NODE_ENV === 'production' ? [
+    plugins: process.env.NODE_ENV === 'production' ? [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin()
   ] : [],
 
-  module: {
-    loaders: [
-      { 
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets:[ 'es2015', 'react', 'stage-2' ]
-        }
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react', 'stage-2']
+                }
       }
     ]
-  }
+    }
 }
