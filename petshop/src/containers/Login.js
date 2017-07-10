731bc7570232254ab32c52ba6ee09f7145264769
antoniop-lm <antoniop.lm@hotmail.com>
2017-07-09 21:48:25 -0300

    Login feito, npm start funcionando

--- petshop/src/containers/Login.js
+++ petshop/src/containers/Login.js
@@ -1,11 +1,31 @@
 import React from 'react'
 
 export default React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object
+  },
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const userName = event.target.elements[0].value
+    const pwd = event.target.elements[1].value
+    const path = `/login`
+    this.context.router.push(path)
+  },
+
   render() {
     return (
-    	<div>
-	    	<h2>Login</h2>
-	    </div>
+      <div>
+				<form id="form-login" onSubmit={this.handleSubmit}>
+						<label>Login:</label>
+						<input type="text"/>
+						<br/> <label>Senha:</label>
+						<input type="password"/>
+						<br/>
+						<button class="submits" id="loginButton" type="submit" onclick="checkAdmin()">Entrar</button>
+				</form>
+        {this.props.children}
+      </div>
     )
   }
 })
